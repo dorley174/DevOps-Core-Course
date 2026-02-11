e9a768650950d2b0cfe441e371480df74e13090c
dorley174 <d.valiev@innopolis.university>
2026-02-11 20:55:19 +0300

    feat: added main lab3 task and added gh ecrets

---
+++ app_python/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from app_python.app import app as flask_app
+
+
+@pytest.fixture()
+def client():
+    flask_app.config.update(
+        TESTING=True,
+    )
+    with flask_app.test_client() as client:
+        yield client
